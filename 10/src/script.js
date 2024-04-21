8aff3c10d9f20ccb0864b79b499febcb619496b4
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-04-21 23:11:04 +0500

    10

--- 10/src/script.js
+++ 10/src/script.js
@@ -1,6 +1,14 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('/textures/door/color.jpg')
+texture.colorSpace = THREE.SRGBColorSpace
+
 /**
  * Base
  */
@@ -14,7 +22,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ map: texture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
