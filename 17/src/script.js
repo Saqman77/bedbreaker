b8487642d404c22ba6e0336a290c772e6122fe78
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-05-19 21:16:17 +0500

    17

--- 17/src/script.js
+++ 17/src/script.js
@@ -18,16 +18,59 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+//Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 20000
 
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+for (let i = 0; i < count * 3; i++)
+    {
+        positions[i] = (Math.random() - 0.5) * 10
+        colors[i] = Math.random()
+    }
+
+    particlesGeometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(positions, 3)
+    )
+
+    particlesGeometry.setAttribute(
+        'color',
+        new THREE.BufferAttribute(colors, 3)
+    )
+
+//Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,
+    color : new THREE.Color('#ff88cc'),
+    transparent: true,
+    alphaMap : particleTexture,
+    // alphaTest : 0.001
+    // depthTest : false
+    depthWrite : false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true
+})
+
+//Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+
+scene.add(particles)
+
+//Cube
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(),
+//     new THREE.MeshBasicMaterial
+// )
+
+// scene.add(cube)
 /**
  * Sizes
  */
@@ -80,6 +123,14 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update particles
+    for(let i = 0; i < count; i++){
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
