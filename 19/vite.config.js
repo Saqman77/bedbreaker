f518e0cd1dd52b93240333258e1141776ea03c41
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-06-23 22:09:48 +0500

    new build

--- 19/vite.config.js
+++ 19/vite.config.js
@@ -1,7 +1,7 @@
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: '/bedbreaker/19/dist/',
     server:
     {
         host: true, // Open to local network and display URL
