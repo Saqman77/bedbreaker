3334fbc8b314dd1a85dacc5d57dc32e1ef8c0149
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-08-21 02:03:09 +0500

    c

--- 23/src/script.js
+++ 23/src/script.js
@@ -53,7 +53,7 @@ scene.add(floor)
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+const ambientLight = new THREE.AmbientLight(0xffffff, .4)
 scene.add(ambientLight)
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
