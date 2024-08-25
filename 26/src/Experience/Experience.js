7bc305f767e24c6676f14c6f860fd1e9a46aae42
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-08-26 02:09:29 +0500

    b

--- 26/src/Experience/Experience.js
+++ 26/src/Experience/Experience.js
@@ -1,9 +1,23 @@
+import * as THREE from 'three'
 import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
+import Camera from './Camera.js'
+import Renderer from './Renderer.js'
+
+let instance = null
+
 export default class Experience
 {
     constructor(canvas)
     {
+
+        if(instance)
+        {
+            return instance
+        }
+        
+        instance = this
+
         // Global access
         window.experience = this
         
@@ -14,6 +28,9 @@ export default class Experience
         // Setup
         this.sizes = new Sizes()
         this.time = new Time()
+        this.scene = new THREE.Scene()
+        this.camera = new Camera()
+        this.renderer = new Renderer()
 
         // Sizes resize event
         this.sizes.on('resize', ()=>
@@ -30,10 +47,13 @@ export default class Experience
 
     resize()
     {
-        console.log('I heared a resize')
+        this.camera.resize()
+        this.renderer.resize()
     }
+
     update()
     {
-        
+        this.camera.update()
+        this.renderer.update()
     }
 }
