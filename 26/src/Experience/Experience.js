65d17f4c204ce0c217036d01ba86125f8a40d367
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-09-02 05:14:48 +0500

    modules setup

--- 26/src/Experience/Experience.js
+++ 26/src/Experience/Experience.js
@@ -32,7 +32,7 @@ export default class Experience
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
-        this.resources = new Resources()
+        this.resources = new Resources(sources)
         this.camera = new Camera()
         this.renderer = new Renderer()
         this.world = new World()
@@ -59,6 +59,7 @@ export default class Experience
     update()
     {
         this.camera.update()
+        this.world.update()
         this.renderer.update()
     }
 }
--- 26/src/Experience/World/Environment.js
+++ 26/src/Experience/World/Environment.js
@@ -7,8 +7,10 @@ export default class Environment
     {
         this.experience = new Experience()
         this.scene = this.experience.scene
+        this.resources = this.experience.resources
 
         this.setSunLight()
+        this.setEnvironementMap()
     }
 
     setSunLight()
@@ -20,5 +22,29 @@ export default class Environment
         this.sunLight.shadow.normalBias = 0.05
         this.sunLight.position.set(3.5, 2, - 1.25)
         this.scene.add(this.sunLight)
+    }
+
+    setEnvironementMap()
+    {
+        this.environmentMap = {}
+        this.environmentMap.intensity = 0.4
+        this.environmentMap.texture = this.resources.items.environmentMapTexture
+        this.environmentMap.texture.colorSpace = THREE.SRGBColorSpace
+
+        this.scene.environment = this.environmentMap.texture
+
+        this.environmentMap.updateMaterials = () =>
+        {
+            this.scene.traverse((child) => 
+                {
+                    if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
+                        {
+                            child.material.envMap = this.environmentMap.texture
+                            child.material.envMapIntensity = this.environmentMap.intensity
+                            child.material.needsUpdate = true
+                        }
+                })
+        }
+        this.environmentMap.updateMaterials()
     }
 }
