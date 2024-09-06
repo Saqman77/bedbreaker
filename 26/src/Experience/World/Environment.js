4b7b26f29cfaae4988642069ef2983104df8581b
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-09-06 10:16:54 +0500

    b

--- 26/src/Experience/World/Environment.js
+++ 26/src/Experience/World/Environment.js
@@ -50,6 +50,25 @@ export default class Environment
                .max(5)
                .step(0.001)
            }
+
+         if(this.debug.active)
+           {
+               this.debugFolder
+               .add(this.sunLight.position, 'y')
+               .name('sunLightY')
+               .min(-5)
+               .max(5)
+               .step(0.001)
+           }
+         if(this.debug.active)
+           {
+               this.debugFolder
+               .add(this.sunLight.position, 'z')
+               .name('sunLightZ')
+               .min(-5)
+               .max(5)
+               .step(0.001)
+           }
     }
 
     setEnvironementMap()
