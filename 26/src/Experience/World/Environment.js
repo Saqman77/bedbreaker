4390f5045829dfcb38fe2158f729498e2ce9b06f
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-09-16 01:22:38 +0500

    done

--- 26/src/Experience/World/Environment.js
+++ 26/src/Experience/World/Environment.js
@@ -8,6 +8,13 @@ export default class Environment
         this.experience = new Experience()
         this.scene = this.experience.scene
         this.resources = this.experience.resources
+        this.debug = this.experience.debug
+
+        // Debug
+        if(this.debug.active)
+        {
+            this.debugFolder = this.debug.ui.addFolder('environment')
+        }
 
         // Setup
         this.setSunLight()
@@ -23,6 +30,24 @@ export default class Environment
         this.sunLight.shadow.normalBias = 0.05
         this.sunLight.position.set(3.5, 2, - 1.25)
         this.scene.add(this.sunLight)
+
+        // Debug
+        if(this.debug.active)
+            {
+                this.debugFolder.add(this.sunLight, 'intensity').name('sunLightintensity').min(0).max(10).step(0.001)
+            }
+        if(this.debug.active)
+            {
+                this.debugFolder.add(this.sunLight.position, 'x').name('sunLightX').min(-5).max(5).step(0.001)
+            }
+        if(this.debug.active)
+            {
+                this.debugFolder.add(this.sunLight.position, 'y').name('sunLightY').min(-5).max(5).step(0.001)
+            }
+        if(this.debug.active)
+            {
+                this.debugFolder.add(this.sunLight.position, 'z').name('sunLightZ').min(-5).max(5).step(0.001)
+            }
     }
 
     setEnvironmentMap()
@@ -50,5 +75,10 @@ export default class Environment
         }
 
         this.environmentMap.updateMaterials()
+        // Debug
+        if(this.debug.active)
+        {
+            this.debugFolder.add(this.environmentMap, 'intensity').name('envMapIntensity').min(0).max(4).step(0.001).onChange(this.environmentMap.updateMaterials)
+        }
     }
 }
