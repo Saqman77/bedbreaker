0e6efd3d63d63d25eab39154dd6e6d895167480f
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-09-24 00:59:22 +0500

    c

--- 27/src/script.js
+++ 27/src/script.js
@@ -1,6 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import testVertexShader from './shaders/test/vertex.glsl'
+import testFragmentShader from './shaders/test/fragment.glsl'
+
+
+
 
 /**
  * Base
@@ -26,7 +31,15 @@ const textureLoader = new THREE.TextureLoader()
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
 // Material
-const material = new THREE.MeshBasicMaterial()
+// const material = new THREE.MeshBasicMaterial()
+
+const material = new THREE.RawShaderMaterial(
+    {
+        vertexShader: testVertexShader,
+        fragmentShader: testFragmentShader,
+        side: THREE.DoubleSide,
+        // wireframe: true
+    })
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
